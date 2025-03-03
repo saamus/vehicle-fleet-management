b069a28aeb05f20e44f30381444b793d2f488aa5
FATIMA ZAHRA FATTOUH <fatima.fattouh@auvergnerhonealpes-ee.fr>
2025-03-03 15:27:35 +0100

    Run Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.js"],  // Lint JavaScript files
+    files: ['**/*.js'], // Lint JavaScript files
     rules: {
-      "semi": ["error", "always"],   // Enforce semicolons at the end of statements
-      "quotes": ["error", "double"], // Enforce double quotes for strings
-      "no-console": "warn",           // Warn on usage of console.log()
-    }
-  }
-];  
+      semi: ['error', 'always'], // Enforce semicolons at the end of statements
+      quotes: ['error', 'double'], // Enforce double quotes for strings
+      'no-console': 'warn', // Warn on usage of console.log()
+    },
+  },
+]
